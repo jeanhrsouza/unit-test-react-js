feba7f5d9a02a9fe3fca861e49cbb17198f9e062
Jean Souza <jeanhrsouza@hotmail.com>
2024-02-18 14:57:23 -0300

    a3: desafio

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -43,15 +43,23 @@ describe('Deve renderizar um campo de input', () => {
 });
 
 test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
-  //Criando uma dublagem do comportamento 'mock'
-  const realizarTransacao = jest.fn();
+  const realizarTransacao = jest.fn(); //Criando uma dublagem do comportamento 'mock'
 
-  //criando renderização
-  render(<Formulario realizarTransacao={realizarTransacao} />);
+  render(<Formulario realizarTransacao={realizarTransacao} />); //criando renderização
+  const botao = screen.getByRole('button'); //criando consulta
 
-  //criando consulta
-  const botao = screen.getByRole('button');
+  userEvent.click(botao); //simulando o click do botão para submeter
+  expect(realizarTransacao).toHaveBeenCalledTimes(1); //verifica se foi chamado, no mínimo, 1x
+});
 
-  userEvent.click(botao);
-  expect(realizarTransacao).toHaveBeenCalledTimes(1);
+test('Deve ser possível selecionar uma opção do elemento <select/>', () => {
+  render(<Formulario />); // renderiza o componente
+  const select = screen.getByRole('combobox'); // faz a consulta do elemento select
+  userEvent.selectOptions(select, ['Depósito']); // simula a ação de selecionar uma opção do select
+
+  expect(
+    screen.getByRole('option', { name: 'Selecione um tipo de transação' })
+      .selected,
+  ).toBe(false); // verifica se a opção de selecionar um tipo de transação não foi selecionada
+  expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(true); // verifica se a opção de depósito foi selecionada
 });
